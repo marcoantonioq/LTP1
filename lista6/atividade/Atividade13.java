e3efbf664e7efb77e2fa39647f6a0f29901b8cb5
Marco Antônio Queiroz <marco.aq7@gmail.com>
2020-10-20 21:16:53 -0300

    14-15

--- lista6/atividade/Atividade13.java
+++ lista6/atividade/Atividade13.java
@@ -1,12 +1,29 @@
-//https://www.youtube.com/watch?v=k_g_QVDrjXE
 package atividade;
 
-
 import java.util.Scanner;  // Import Scanner class
-import java.lang.Math;
 
 public class Atividade13 { // class
     public Atividade13(){ // constrututor
+
+        Scanner input = new Scanner(System.in);
+
+        int valor;
+
+        System.out.println("Informe um valor: ");
+        // valor = input.nextInt();
+        valor=20;
+
+        System.out.print(valor+" ");
+
+        if(valor > 0 && valor < 25){
+            System.out.println("Está entre 0 e 25");
+        } else if(valor >= 25 && valor < 50) {
+            System.out.println("Está entre 25 e 50");
+        } else if(valor >= 50 && valor < 75) {
+            System.out.println("Está entre 50 e 75");
+        } else if(valor >= 75 && valor < 100) {
+            System.out.println("Está entre 75 e 100");
+        }
 
     }
 }
---
+++ lista6/atividade/Atividade14.java
@@ -0,0 +1,33 @@
+package atividade;
+
+import java.util.Scanner;  // Import Scanner class
+
+public class Atividade14 { // class
+    public Atividade14(){ // constrututor
+        
+        Scanner input = new Scanner(System.in);
+        
+        double A,B,C;
+        
+        System.out.print("Informa lado A: ");
+        A = input.nextDouble();
+        
+        System.out.print("Informa lado B: ");
+        B = input.nextDouble();
+        
+        System.out.print("Informa lado C: ");
+        C = input.nextDouble();
+        
+        if(A == 0 || B == 0 || C == 0){
+            System.out.println("Valores não pode ser 0");
+        } else {
+            if(C > A + B || A > B + C || B > A + C) {
+                System.out.println("Perimetro = " + (A+B+C));
+            } else {
+                System.out.println("Não é um triangulo");
+                System.out.println("Área do trapézio: " + ((C+B)/2)*A );
+            }
+        }
+        
+    }
+}
